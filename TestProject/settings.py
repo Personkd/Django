03e30365c470605dc6faa017d5f1126e6870249a
Personkd <153018019+Personkd@users.noreply.github.com>
2024-05-28 21:43:39 +0300

    Trying to fix №2

--- TestProject/settings.py
+++ TestProject/settings.py
@@ -119,9 +119,19 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
+
 STATIC_URL = 'static/'
+
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
+
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'instagram/static')
+]
+
+
 MEDIA_URL = "images/"
 MEDIA_ROOT = os.path.join(BASE_DIR,"static/images")
 
